f39f9ffc832034c643de894e599a1a3cf3bc0add
lyhisproving <625101721@qq.com>
2020-10-31 17:34:45 +0800

    runable 2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import numpy as np
-from torch import nn,Tensor
+from torch import nn, Tensor
 import torch
 import torch.nn.functional as F
+
+
 def one_hot(label, num_class):
     label = np.array(label)
     num_label = label.shape[0]
@@ -11,6 +13,7 @@ def one_hot(label, num_class):
     out.flat[index + label.ravel()] = 1
     return out
 
+
 def seq_padding(X, padding=0):
     L = [len(x) for x in X]
     ML = max(L)
@@ -18,6 +21,7 @@ def seq_padding(X, padding=0):
         np.concatenate([x, [padding] * (ML - len(x))]) if len(x) < ML else x
         for x in X
     ])
+
 
 def load_data(train_file, test_file, train_label, test_label):
     #params: train_file,test_file
@@ -32,18 +36,18 @@ def load_data(train_file, test_file, train_label, test_label):
     x_test_out = []
     y_train = []
     y_test = []
-    max_int_seq_size=0
-    max_out_seq_size=0
+    max_int_seq_size = 0
+    max_out_seq_size = 0
     with open(train_file, 'r') as f:
         line = f.readline()
         while line:
             lt = line.split('#')
             lt_1 = lt[0].split()
             lt_2 = lt[1].split()
-            max_int_seq_size=max(max_int_seq_size,len(lt_1))
-            max_out_seq_size=max(max_out_seq_size,len(lt_2))
+            max_int_seq_size = max(max_int_seq_size, len(lt_1))
+            max_out_seq_size = max(max_out_seq_size, len(lt_2))
             data_in = [int(x) for x in lt_1]
-            data_out=[int(x) for x in lt_2]
+            data_out = [int(x) for x in lt_2]
             x_train_in.append(data_in)
             x_train_out.append(data_out)
             #lst = lt_1+lt_2
@@ -57,8 +61,8 @@ def load_data(train_file, test_file, train_label, test_label):
             lt = line.split('#')
             lt_1 = lt[0].split()
             lt_2 = lt[1].split()
-            max_int_seq_size=max(max_int_seq_size,len(lt_1))
-            max_out_seq_size=max(max_out_seq_size,len(lt_2))
+            max_int_seq_size = max(max_int_seq_size, len(lt_1))
+            max_out_seq_size = max(max_out_seq_size, len(lt_2))
             data_in = [int(x) for x in lt_1]
             data_out = [int(x) for x in lt_2]
             x_test_in.append(data_in)
@@ -82,100 +86,101 @@ def load_data(train_file, test_file, train_label, test_label):
             data = [int(x) for x in lst]
             y_test.append(data)
             line = f.readline()
-    
-    X_in = x_train_in+x_test_in
-    X_out=x_train_out+x_test_out
-    X_in=seq_padding(X_in)
+
+    X_in = x_train_in + x_test_in
+    X_out = x_train_out + x_test_out
+    X_in = seq_padding(X_in)
     X_out = seq_padding(X_out)
-    X_in=np.array(X_in)
-    X_out=np.array(X_out)
-    x_train_in=X_in[0:len(x_train_in)]
-    x_test_in=X_in[len(x_train_in):]
-    x_train_out=X_out[0:len(x_train_out)]
-    x_test_out=X_out[len(x_train_out):]
+    X_in = np.array(X_in)
+    X_out = np.array(X_out)
+    x_train_in = X_in[0:len(x_train_in)]
+    x_test_in = X_in[len(x_train_in):]
+    x_train_out = X_out[0:len(x_train_out)]
+    x_test_out = X_out[len(x_train_out):]
 
     y_train = np.array(y_train)
     y_test = np.array(y_test)
-    x_train_in = x_train_in.reshape(x_train_in.shape[0], x_train_in.shape[1], 1)
+    x_train_in = x_train_in.reshape(x_train_in.shape[0], x_train_in.shape[1],
+                                    1)
     x_test_in = x_test_in.reshape(x_test_in.shape[0], x_test_in.shape[1], 1)
-    x_train_out = x_train_out.reshape(x_train_out.shape[0],x_train_out.shape[1],1)
-    x_test_out = x_test_out.reshape(x_test_out.shape[0],x_test_out.shape[1],1)
+    x_train_out = x_train_out.reshape(x_train_out.shape[0],
+                                      x_train_out.shape[1], 1)
+    x_test_out = x_test_out.reshape(x_test_out.shape[0], x_test_out.shape[1],
+                                    1)
 
     y_train = y_train.reshape(y_train.shape[0], 1)
     y_test = y_test.reshape(y_test.shape[0], 1)
     #print(np.shape(x_train),np.shape(x_test))
-    return x_train_in, x_train_out, x_test_in,x_test_out,y_train, y_test
+    return x_train_in, x_train_out, x_test_in, x_test_out, y_train, y_test
 
 
-def model_1(x_in,x_out,hidden_size):
+def model_1(x_in, x_out, hidden_size):
     with tf.variable_scope('LSTM_1'):
         cell_in_1 = tf.contrib.rnn.BasicLSTMCell(num_units=hidden_size)
-        outputs_in,states_in = tf.nn.dynamic_rnn(
-            cell = cell_in_1,
-            inputs = x_in,
-            time_major = False,
-            dtype = tf.float32,
+        outputs_in, states_in = tf.nn.dynamic_rnn(
+            cell=cell_in_1,
+            inputs=x_in,
+            time_major=False,
+            dtype=tf.float32,
         )
-    with tf.variable_scope('LSTM_2',reuse=tf.AUTO_REUSE):
+    with tf.variable_scope('LSTM_2', reuse=tf.AUTO_REUSE):
         cell_out_1 = tf.nn.rnn_cell.BasicLSTMCell(num_units=hidden_size)
-        outputs_out,states_out = tf.nn.dynamic_rnn(
+        outputs_out, states_out = tf.nn.dynamic_rnn(
             cell=cell_out_1,
-            inputs = x_out,
-            time_major = False,
-            dtype = tf.float32,
+            inputs=x_out,
+            time_major=False,
+            dtype=tf.float32,
         )
-    return outputs_in,outputs_out
+    return outputs_in, outputs_out
 
-def model_2(outputs_in,outputs_out,hidden_size):
+
+def model_2(outputs_in, outputs_out, hidden_size):
     with tf.name_scope('coAttention'):
-        dense = tf.layers.Dense(hidden_size,activation='relu')
+        dense = tf.layers.Dense(hidden_size, activation='relu')
         x = dense(outputs_in)
-        matrix = tf.matmul(x,tf.transpose(outputs_out,perm=[0,2,1]))
+        matrix = tf.matmul(x, tf.transpose(outputs_out, perm=[0, 2, 1]))
         #input_matrix = mask.unsqueeze(1)
         #output_matrix = mask.unsqueeze(-1)
-        input_weight = tf.nn.softmax(matrix,dim=1)
-        output_weight = tf.nn.softmax(matrix,dim=-1)
+        input_weight = tf.nn.softmax(matrix, dim=1)
+        output_weight = tf.nn.softmax(matrix, dim=-1)
         #(batch_size,seq_len,hidden_size)
-        coat_in = tf.matmul(input_weight,outputs_out)
-        coat_out = tf.matmul(tf.transpose(output_weight,perm=[0,2,1]),outputs_in)
-        co_in_result = tf.concat((coat_in,outputs_in),-1)
-        co_out_result = tf.concat((coat_out,outputs_out),-1)
-    return co_in_result,co_out_result
+        coat_in = tf.matmul(input_weight, outputs_out)
+        coat_out = tf.matmul(tf.transpose(output_weight, perm=[0, 2, 1]),
+                             outputs_in)
+        co_in_result = tf.concat((coat_in, outputs_in), -1)
+        co_out_result = tf.concat((coat_out, outputs_out), -1)
+    return co_in_result, co_out_result
 
-def model_3(co_in_result,co_out_result,hidden_size):
-    with tf.variable_scope('LSTM_3',reuse=tf.AUTO_REUSE):
+
+def model_3(co_in_result, co_out_result, hidden_size):
+    with tf.variable_scope('LSTM_3', reuse=tf.AUTO_REUSE):
         cell_in_2 = tf.nn.rnn_cell.BasicLSTMCell(num_units=hidden_size)
-        output_final_in,state_final_in = tf.nn.dynamic_rnn(
-            cell = cell_in_2,
-            inputs = co_in_result,
-            time_major = False,
-            dtype = tf.float32
-        )
-    with tf.variable_scope('LSTM_4',reuse=tf.AUTO_REUSE):
+        output_final_in, state_final_in = tf.nn.dynamic_rnn(
+            cell=cell_in_2,
+            inputs=co_in_result,
+            time_major=False,
+            dtype=tf.float32)
+    with tf.variable_scope('LSTM_4', reuse=tf.AUTO_REUSE):
         cell_out_2 = tf.nn.rnn_cell.BasicLSTMCell(num_units=hidden_size)
-        output_final_out,state_final_out = tf.nn.dynamic_rnn(
+        output_final_out, state_final_out = tf.nn.dynamic_rnn(
             cell=cell_out_2,
-            inputs = co_out_result,
-            time_major = False,
-            dtype = tf.float32
-        )
-    return output_final_in,output_final_out
+            inputs=co_out_result,
+            time_major=False,
+            dtype=tf.float32)
+    return output_final_in, output_final_out
 
-    
+
 def main():
-    x_train_in, x_train_out, x_test_in,x_test_out,y_train,y_test = load_data('data/TrainSamples_10_26.txt',
-                                             'data/TestSamples_10_26.txt',
-                                             'data/TrainLabel_10_26.txt',
-                                             'data/TestLabel_10_26.txt')
+    x_train_in, x_train_out, x_test_in, x_test_out, y_train, y_test = load_data(
+        'data/TrainSamples_10_26.txt', 'data/TestSamples_10_26.txt',
+        'data/TrainLabel_10_26.txt', 'data/TestLabel_10_26.txt')
     batch_size = 64
     seq_len_in = x_train_in.shape[1]
     seq_len_out = x_train_out.shape[1]
-    print(type(seq_len_in),seq_len_in)
-    print(type(seq_len_out),seq_len_out)
     embed_size = 1
-    learning_rate = 0.005
-    hidden_size = 64
-    iterations = 5000
+    learning_rate = 0.0001
+    hidden_size = 128
+    iterations = 3000
     n_classes = 2
     y_train = one_hot(y_train, n_classes)  #(200000,2)
     y_test = one_hot(y_test, n_classes)  #(40000,2)
@@ -186,40 +191,90 @@ def main():
     sess1 = tf.Session(graph=g1)
     with sess1.as_default():
         with g1.as_default():
-            x_in_place = tf.placeholder(tf.float32, [None, embed_size * seq_len_in])
+            x_in_place = tf.placeholder(tf.float32,
+                                        [None, embed_size * seq_len_in])
             x_in_place = tf.reshape(x_in_place, [-1, seq_len_in, embed_size])
-            x_out_place = tf.placeholder(tf.float32,[None,embed_size* seq_len_out])
-            x_out_place = tf.reshape(x_out_place,[-1,seq_len_out,embed_size])
+            x_out_place = tf.placeholder(tf.float32,
+                                         [None, embed_size * seq_len_out])
+            x_out_place = tf.reshape(x_out_place,
+                                     [-1, seq_len_out, embed_size])
             y_place = tf.placeholder(tf.int32, [None, n_classes])
             #mask_place = tf.placeholder(tf.float32,[None,embed_size])
-            output_in,output_out = model_1(x_in_place,x_out_place,hidden_size)
-            co_in_result,co_out_result = model_2(output_in,output_out,hidden_size)
-            outputs_final_in,outputs_final_out = model_3(co_in_result,co_out_result,hidden_size)
-            output_final = outputs_final_in+outputs_final_out
-            output = tf.layers.dense(inputs=output_final[:,-1,:],units=n_classes,activation=tf.nn.softmax)
+            output_in, output_out = model_1(x_in_place, x_out_place,
+                                            hidden_size)
+            '''co_in_result, co_out_result = model_2(output_in, output_out,
+                                                  hidden_size)
+            outputs_final_in, outputs_final_out = model_3(
+                co_in_result, co_out_result, hidden_size)
+            output_final = outputs_final_in + outputs_final_out'''
+            output_final = output_out
+            output = tf.layers.dense(inputs=output_final[:, -1, :],
+                                     units=n_classes,
+                                     activation=tf.nn.softmax)
             with tf.name_scope('loss'):
-                cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_place,logits=output)
+                cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+                    labels=y_place, logits=output)
                 cross_entropy = tf.reduce_mean(cross_entropy)
             with tf.name_scope('adam_optimizer'):
-                train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+                train_step = tf.train.AdamOptimizer(
+                    learning_rate=learning_rate).minimize(cross_entropy)
             with tf.name_scope('accuracy'):
-                correct_prediction = tf.equal(tf.arg_max(y_place,1),tf.arg_max(output,1))
-                correct_prediction = tf.cast(correct_prediction,tf.float32)
+                correct_prediction = tf.equal(tf.argmax(y_place, axis=1),
+                                              tf.argmax(output, axis=1))
+                correct_prediction = tf.cast(correct_prediction, tf.float32)
                 accuracy = tf.reduce_mean(correct_prediction)
+                '''prediction = tf.argmax(output, axis=1)
+                actual = tf.argmax(y_place, axis=1)
+                ones_like_actual = tf.ones_like(y_place)
+                zeros_like_actual = tf.zeros_like(y_place)
+                ones_like_prediction = tf.ones_like(prediction)
+                zeros_like_prediction = tf.zeros_like(prediction)
+                tp_op = tf.reduce_sum(
+                    tf.cast(
+                        tf.logical_and(
+                            tf.equal(actual, ones_like_actual),
+                            tf.equal(prediction, ones_like_prediction)),
+                        'float'))
+                tn_op = tf.reduce_sum(
+                    tf.cast(
+                        tf.logical_and(
+                            tf.equal(actual, zeros_like_actual),
+                            tf.equal(prediction, zeros_like_prediction),
+                        ), 'float'))
+                fp_op = tf.reduce_sum(
+                    tf.cast(
+                        tf.logical_and(
+                            tf.equal(actual, zeros_like_actual),
+                            tf.equal(prediction, ones_like_prediction)),
+                        'float'))
+                fn_op = tf.reduce_sum(
+                    tf.cast(
+                        tf.logical_and(
+                            tf.equal(actual, ones_like_actual),
+                            tf.equal(prediction, zeros_like_prediction)),
+                        'float'))
+                #precision = '''
             init = tf.global_variables_initializer()
         sess1.run(init)
     index = 0
     for step in range(iterations):
         X_train_in = x_train_in[index:index + batch_size, :, :]
         X_train_out = x_train_out[index:index + batch_size, :, :]
-        mask_train = y_train[index:index+batch_size,:]
+        mask_train = y_train[index:index + batch_size, :]
         Y_train = y_train[index:index + batch_size, :]
         index += batch_size
-        loss_,output_,accuracy_ = sess1.run([cross_entropy,output,accuracy],feed_dict={x_in_place:X_train_in,x_out_place:X_train_out,y_place:Y_train})
-        
-
-        print(accuracy_,loss_,output_)
+        loss_, output_, accuracy_ = sess1.run(
+            [cross_entropy, output, accuracy],
+            feed_dict={
+                x_in_place: X_train_in,
+                x_out_place: X_train_out,
+                y_place: Y_train
+            })
+        if step % 100 == 0:
+            print(accuracy_, loss_, step)
     return 1
+
+
 if __name__ == '__main__':
     main()
 '''x_in_place = tf.placeholder(tf.float32, [None, embed_size * seq_len_in])
@@ -364,4 +419,3 @@ for step in range(iterations):
               '\tprecision:%f' % precision, '\tstep: %d' % step)
         #print('success')
 '''
-
